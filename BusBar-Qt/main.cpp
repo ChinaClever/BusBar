99fecd1e2d08c067583e8051070a7c73e507f2bf
luozhiyong <luozhiyong131@qq.com>
2020-09-25 17:05:47 +0800

    增加权限

--- BusBar-Qt/main.cpp
+++ BusBar-Qt/main.cpp
@@ -4,34 +4,34 @@
 #include "frmnum.h"
 //#include "permissions.h"
 
-//#include <QAndroidJniObject>
-//#include <QAndroidJniEnvironment>
-//#include <QtAndroid>
+#include <QAndroidJniObject>
+#include <QAndroidJniEnvironment>
+#include <QtAndroid>
 
-//bool checkPermission() {
-//    QStringList list;
-//    list << "android.permission.READ_EXTERNAL_STORAGE";
-//    list << "android.permission.WRITE_EXTERNAL_STORAGE";
-//    list << "android.permission.MOUNT_UNMOUNT_FILESYSTEMS";
+bool checkPermission() {
+    QStringList list;
+    list << "android.permission.READ_EXTERNAL_STORAGE";
+    list << "android.permission.WRITE_EXTERNAL_STORAGE";
+    list << "android.permission.MOUNT_UNMOUNT_FILESYSTEMS";
 
-//    for(int i=0; i<list.size(); ++i) {
-//        QtAndroid::PermissionResult r = QtAndroid::checkPermission(list.at(i));
-//        if(r == QtAndroid::PermissionResult::Denied) {
-//            QtAndroid::requestPermissionsSync( QStringList() << list.at(i) );
-//            r = QtAndroid::checkPermission(list.at(i));
-//            if(r == QtAndroid::PermissionResult::Denied) {
-//                return false;
-//            }
-//        }
-//    }
+    for(int i=0; i<list.size(); ++i) {
+        QtAndroid::PermissionResult r = QtAndroid::checkPermission(list.at(i));
+        if(r == QtAndroid::PermissionResult::Denied) {
+            QtAndroid::requestPermissionsSync( QStringList() << list.at(i) );
+            r = QtAndroid::checkPermission(list.at(i));
+            if(r == QtAndroid::PermissionResult::Denied) {
+                return false;
+            }
+        }
+    }
 
-//    return true;
-//}
+    return true;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//    checkPermission();
+    checkPermission();
 
     MainWindow w;
     w.showFullScreen();

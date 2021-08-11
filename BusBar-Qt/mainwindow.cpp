02f0b8e0246c29d84edecd430a6536f6929dd605
luozhiyong131 <luozhiyong131@qq.com>
2021-08-11 15:11:55 +0800

    2.0.3

--- BusBar-Qt/mainwindow.cpp
+++ BusBar-Qt/mainwindow.cpp
@@ -104,8 +104,8 @@ void MainWindow::setBusName(int index)
     sBusData *busData = &(shm->data[index]);
     double rateCur = busData->box[0].ratedCur/COM_RATE_CUR;
     ui->ratedLab->setText(QString::number(rateCur));
-//    ui->ratedLab->setText("V2.0.2");//上海创建
-    ui->ratedLab->setText("V2.0.0");
+    ui->ratedLab->setText("V2.0.3");//上海创建
+//     ui->ratedLab->setText("V2.0.0");
 }
 
 void MainWindow::checkAlarm()
